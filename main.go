5c2077bf63ccb2d92014ca1fb6123f3ea1b0caa1
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-06-05 23:20:48 -0300

    init commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const tshirt = 43.99
+const short = 50.00
+const sneaker = 130.00
+const hoodie = 199.99
+const jeans = 149.99
+const sandal = 19.99
+
+func main() {
+	fmt.Println("----------------SHOP----------------")
+	fmt.Println("T-SHIRT 		R$ 43,99")
+	fmt.Println("SHORT   		R$ 50,00")
+	fmt.Println("SNEAKER 		R$ 130,00")
+	fmt.Println("HOODIE 			R$ 199,99")
+	fmt.Println("JEANS			R$ 149,99")
+	fmt.Println("SANDAL			R$ 19,99")
+	fmt.Println("----------------END-----------------")
+}
+
+func buy() {
+	const tshirt = 43.99
+	const short = 50.00
+	const sneaker = 130.00
+	const hoodie = 199.99
+	const jeans = 149.99
+	const sandal = 19.99
+	fmt.Println("You purchased the item by R$")
+}
