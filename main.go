11a0498f2f92e2e85126c2e75f8aedaf193aa67f
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-06-07 01:34:57 -0300

    feat(shop): finished the shop

--- main.go
+++ main.go
@@ -18,14 +18,35 @@ func main() {
 	fmt.Println("JEANS			R$ 149,99")
 	fmt.Println("SANDAL			R$ 19,99")
 	fmt.Println("----------------END-----------------")
-}
 
-func buy() {
-	const tshirt = 43.99
-	const short = 50.00
-	const sneaker = 130.00
-	const hoodie = 199.99
-	const jeans = 149.99
-	const sandal = 19.99
-	fmt.Println("You purchased the item by R$")
+	buy := func(item string) {
+
+		if item == "tshirt" {
+			fmt.Println("You purchased the t-shirt with R$ ", tshirt)
+		}
+
+		if item == "short" {
+			fmt.Println("You purchased the short with R$ ", short)
+		}
+
+		if item == "sneaker" {
+			fmt.Println("You purchased the sneaker with R$ ", sneaker)
+		}
+
+		if item == "hoodie" {
+			fmt.Println("You purchased the hoodie with R$ ", hoodie)
+		}
+
+		if item == "jeans" {
+			fmt.Println("You purchased the jeans with R$ ", jeans)
+		}
+
+		if item == "sandal" {
+			fmt.Println("You purchased the sandal with R$ ", sandal)
+		}
+
+	}
+
+	buy("jeans")
+
 }
